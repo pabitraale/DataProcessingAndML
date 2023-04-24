f8253eb850e6fb188c410c90f37422290d6498cc
pabitraale <pabitraale25@yahoo.com>
2023-04-24 14:10:07 -0400

    test

--- Script/Main.py
+++ Script/Main.py
@@ -1,1 +1,16 @@
-print("Hello, World!!")
+import vtk
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+print("hello world")
+
+a = np.arange(15).reshape(3,5)
+print(a)
+
+plt.plot([1,2,3,4])
+plt.ylabel('some number')
+plt.show()
+
+reader = vtk.vtkUnstructuredGridReader()
+reader.SetFileName()

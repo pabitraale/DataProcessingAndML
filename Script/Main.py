c87b03fae52b3e4fb2748316d7b6c9ff35f1d5be
pabitraale <pabitraale25@yahoo.com>
2023-04-24 16:33:31 -0400

    read file

--- Script/Main.py
+++ Script/Main.py
@@ -1,20 +1,62 @@
 import vtk
-<<<<<<< HEAD
 import numpy as np
 import matplotlib.pyplot as plt
+from vtkmodules.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
 
-print("hello world")
+'''print("hello world")
 
 a = np.arange(15).reshape(3,5)
 print(a)
 
 plt.plot([1,2,3,4])
 plt.ylabel('some number')
-plt.show()
+plt.show()'''
 
+#open file and read unstructured grid
 reader = vtk.vtkUnstructuredGridReader()
-reader.SetFileName()
-=======
+reader.SetFileName("Data\cavity-coarse_100.vtk")
+reader.Update()
+grid = reader.GetOutput()
+print(grid)
+#get number of cells
+print("number of cell: ", grid.GetNumberOfCells())
 
->>>>>>> 9d32b53eb0cd6644b40f57b174b9f815ae4a138f
+#read points
+vtk_points = grid.GetPoints()
+print(vtk_points.GetData())
+xyz3d = vtk_to_numpy(vtk_points.GetData())
+print(xyz3d)
+#print(xyz3d.size)
+
+print("get cell arrys")
+print("=============================")
+
+print("==========================================")
+
+
+#cell data 
+print("printing cell data")
+cell_data = grid.GetCellData()
+cell_arrya = cell_data.GetArray('cellID')
+print(vtk_to_numpy(cell_arrya))
+print('\n')
+
+cell_data_p = cell_data.GetArray('p')
+print(vtk_to_numpy(cell_data_p))
+print('\n')
+
+cell_data_u = cell_data.GetArray('U')
+print(vtk_to_numpy(cell_data_u))
+print('\n')
+
+#point data
+print("printing point data")
+point_data = grid.GetPointData()
+points_data_p = point_data.GetArray('p')
+print(vtk_to_numpy(points_data_p))
+
+print('\n')
+points_data_u = point_data.GetArray('U')
+print(vtk_to_numpy(points_data_u))
+

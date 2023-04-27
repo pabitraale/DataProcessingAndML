1bc6a6c8a7c39ee4f766213afe2c7b5857371c70
pabitraale <pabitraale25@yahoo.com>
2023-04-27 09:23:45 -0400

    print cell type

--- Script/Main.py
+++ Script/Main.py
@@ -4,48 +4,94 @@ import matplotlib.pyplot as plt
 from vtkmodules.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
 
-'''print("hello world")
-
-a = np.arange(15).reshape(3,5)
-print(a)
-
-plt.plot([1,2,3,4])
-plt.ylabel('some number')
-plt.show()'''
-
 #open file and read unstructured grid
 reader = vtk.vtkUnstructuredGridReader()
 reader.SetFileName("Data\cavity-coarse_100.vtk")
 reader.Update()
 grid = reader.GetOutput()
-print(grid)
+
 #get number of cells
-print("number of cell: ", grid.GetNumberOfCells())
+numberOfCellsInGrid = grid.GetNumberOfCells()
+print("number of cell: ", numberOfCellsInGrid)
 
 #read points
 vtk_points = grid.GetPoints()
-print(vtk_points.GetData())
 xyz3d = vtk_to_numpy(vtk_points.GetData())
-print(xyz3d)
-#print(xyz3d.size)
 
+print("number of points: ", grid.GetNumberOfPoints())
+print('POINTS  %s  float'% (grid.GetNumberOfPoints()))
+print(xyz3d)
+'''i = 0
+while(i < grid.GetNumberOfPoints()):
+    print("Next point: %d, %s " % (i+1, grid.GetPoint(i)))
+    i = i+1'''
+
+#find cell id
+verticesInSingleCell = []
+# print index 0 to find out the total number of vertices in each cell
+for i in range(0, numberOfCellsInGrid):
+    currentCell = grid.GetCell(i)
+    verticesInSingleCell.append(currentCell.GetNumberOfPoints())
+print("\nTotal vetices in each cell")
+print(verticesInSingleCell)
+
+#get vertices of each cell and store in cellConnectivityPoints
+#cellConnectivityPoints = []
+cellVertices = []
+
+for i in range ( 0, numberOfCellsInGrid):
+    currentCell = grid.GetCell(i)
+    #print(currentCell.GetNumberOfPoints())
+    arr = [currentCell.GetNumberOfPoints()]
+    print("array: ", arr) 
+    for j in range (0, currentCell.GetNumberOfPoints()):
+        tempPointStore = currentCell.GetPointId(j)
+        #print(" temp: ", tempPointStore)
+        #cellConnectivityPoints.append(tempPointStore )
+        arr.append(tempPointStore)# = np.array(cellConnectivityPoints)
+    
+    cellVertices.append(arr)
+    
+#print(cellVertices[0])       
+for i in range (0, numberOfCellsInGrid):
+    print(cellVertices[i])
+   
+
+print('\nCELL %s ' % numberOfCellsInGrid)
 print("get cell arrys")
 print("=============================")
+# get cell 0 and print it's types 
+cell0 = grid.GetCell(0)
+print("cell types: ", cell0.GetCellType()) #prints cell types
+print("first point: ", cell0.GetNumberOfPoints()) #print first point
+print("=============================")
+#get total points of cells (CELL 4 36)
 
-print("==========================================")
+print("total points: ", grid.GetNumberOfPoints())
 
 
+
+#create a list to store cell types
+storeCellTypes = []
+for i in range (0, numberOfCellsInGrid):
+    storeCellTypes.append(grid.GetCellType(i))
+print("cell types: ", storeCellTypes)
+
+  
 #cell data 
 print("printing cell data")
+print("cellID 1 4 int")
 cell_data = grid.GetCellData()
-cell_arrya = cell_data.GetArray('cellID')
-print(vtk_to_numpy(cell_arrya))
+cell_array = cell_data.GetArray('cellID')
+print(vtk_to_numpy(cell_array))
 print('\n')
 
+print("p 1 float")
 cell_data_p = cell_data.GetArray('p')
 print(vtk_to_numpy(cell_data_p))
 print('\n')
 
+print("U 3 4 float")
 cell_data_u = cell_data.GetArray('U')
 print(vtk_to_numpy(cell_data_u))
 print('\n')
@@ -54,9 +100,11 @@ print('\n')
 print("printing point data")
 point_data = grid.GetPointData()
 points_data_p = point_data.GetArray('p')
+print("p 1 18 float")
 print(vtk_to_numpy(points_data_p))
 
 print('\n')
 points_data_u = point_data.GetArray('U')
+print("U 3 18 float")
 print(vtk_to_numpy(points_data_u))
 
